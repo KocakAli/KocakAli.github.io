b55bbe72dc3d6ef8daa2e78916a191b9d9195a2b
Dreamsoldier <kocakali5834@gmail.com>
2022-09-13 00:41:11 +0300

    finish startgame and checkanswer functions

--- guess-the-number.js
+++ guess-the-number.js
@@ -10,26 +10,88 @@ const startGame = ()=>{
     console.log(rndmNumber);
     const inp = document.querySelector('input');
     const btn = document.querySelector('button');
-    let chance = 10;
+    let chance = 1;
 
-    btn.addEventListener('click', ()=>{
+    checkAnswer(inp,btn,chance,rndmNumber);
+
+}
+
+
+
+
+const checkAnswer = (inp,btn,chance,rndmNumber)=>{
+   btn.addEventListener('click', ()=>{
         const data = inp.value;
 
-        if(data == rndmNumber){
+        
+
+        if(chance == 10){
+            console.log('game over')}
+        if(data > 100 || data < 1){
+            alert('number must be between 1 and 100');
+            inp.textContent = "";
+            focus(inp);
+        }else if(data == rndmNumber){
             const tb = document.querySelector('tbody');
             const row = document.createElement('tr');
             const td1 = document.createElement('td');
             const td2 = document.createElement('td');
-            td1.textContent = data;
-            td2.textContent = "Correct Answer";
 
+
+            td1.textContent = data;
+            td1.style.fontWeight = "bold";
+            td2.textContent = "Correct Answer";
+            td2.style.color = "green";
+            td2.style.fontWeight = "bold"
+            const td3 = document.createElement('td');
+            td3.textContent = `${chance}.`;
+            td3.style.color = "orange";
+            td3.style.fontWeight = "bold";
+
+
+            row.appendChild(td3);
             row.appendChild(td1);
             row.appendChild(td2);
-
             tb.appendChild(row);
+
+        }else{
+            const tb = document.querySelector('tbody');
+            const row = document.createElement('tr');
+            const td1 = document.createElement('td');
+            const td2 = document.createElement('td');
+
+            td1.textContent = data;
+            td1.style.fontWeight = "bold";
+
+            td2.style.color = "red";
+            td2.style.fontWeight = "bold";
+
+            
+            if(data > rndmNumber){    
+                td2.textContent = "Your guess is higher than the number"; 
+            }else if(data < rndmNumber){
+                td2.textContent = "Your guess is lower than the number"; 
+            }
+            
+
+            const td3 = document.createElement('td');
+            td3.textContent = `${chance}.`;
+            td3.style.color = "orange";
+            td3.style.fontWeight = "bold";
+
+            row.appendChild(td3);
+            row.appendChild(td1);
+            row.appendChild(td2);
+            tb.appendChild(row);
+            console.log("check");
+            chance++;
+            inp.value = "";
+            inp.focus();
         }
-    
     });
  
-
 }
+
+    
+ 
+
